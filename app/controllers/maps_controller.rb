8ec0664d5ac215e9ab8eeb9040d365b29184749c
akerue <hsmgn8118akerue@gmail.com>
2014-11-20 01:57:01 +0900

    Add function to judge which direction you can move to

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,12 +6,28 @@ class MapsController < ApplicationController
     @photo = generate_photo(Photo.find_by(x: params[:x], y: params[:y], z: params[:z], theta: params[:theta]))
     render json: @photo
   end
+  
+  def feel_all_around
+    x = params[:x].to_i
+    y = params[:y].to_i
+    z = params[:z].to_i
+
+    flags = {
+      forward: Photo.find_by(x: x, y: y+1, z: z).present?,
+      back: Photo.find_by(x: x, y: y-1, z: z).present?,
+      left: Photo.find_by(x: x-1, y: y, z: z).present?,
+      right: Photo.find_by(x: x+1, y: y, z: z).present?
+    }
+    render json: flags
+  end
 
   private
+  # 写真が無かった場合にダミーのphotoオブジェクト作成
   def generate_photo(photo)
     unless photo
-      photo = Photo.new(x: nil, y: nil, z: nil, theta: nil, path: nil)
+      photo = Photo.new(x: nil, y: nil, z: nil, theta: nil, path: "/images/no-image.png")
     end
     photo
   end
+
 end
